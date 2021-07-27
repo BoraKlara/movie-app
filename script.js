5b58cce64b1ab6d39b87e0d2e47eb4f0e7f1e955
B. Klara Simon <borbala.klara@gmail.com>
2021-07-27 22:07:02 +0200

    add api

---
+++ script.js
@@ -0,0 +1,7 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=263eeb246a0c04778be8543d7bc70709&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_URL =
+  "https://api.themoviedb.org/3/discover/movie?api_key=263eeb246a0c04778be8543d7bc70709&query=''";
