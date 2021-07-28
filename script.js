d455087425694fb19d739ee9debbd0ba8449fbbc
B. Klara Simon <borbala.klara@gmail.com>
2021-07-28 23:17:18 +0200

    add API

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_URL =
   'https://api.themoviedb.org/3/discover/movie?api_key=263eeb246a0c04778be8543d7bc70709&query="';
 
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 getMovies(API_URL);
 
 async function getMovies(url) {
@@ -14,3 +17,13 @@ async function getMovies(url) {
 
   console.log(data.results);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.nodeValue;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_URL);
+  }
+});
